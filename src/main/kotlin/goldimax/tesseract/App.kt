91ca60fdfe6819d24d6b44c44be1cb3e38c48312
billow <billow.fun@gmail.com>
2020-04-27 08:22:49 +0800

    Remove proxy settings

--- src/main/kotlin/goldimax/tesseract/App.kt
+++ src/main/kotlin/goldimax/tesseract/App.kt
@@ -4,9 +4,6 @@ import kotlinx.coroutines.runBlocking
 import org.apache.log4j.BasicConfigurator
 
 fun main() = runBlocking<Unit> {
-//    System.setProperty("java.net.useSystemProxies", "true");
-//    System.setProperty("socksProxyHost", "192.168.1.6");
-//    System.setProperty("socksProxyPort", "7891");
 //    Log4j configure
     BasicConfigurator.configure()
     val bot = UniBot("conf.json")
