2346b1f799e6cd2867a9a8238f87e82b3dcb712e
eman0n <EmanonTang@outlook.com>
2021-11-02 01:55:55 +0800

    support multi group recording

--- src/main/kotlin/goldimax/tesseract/Recorder.kt
+++ src/main/kotlin/goldimax/tesseract/Recorder.kt
@@ -9,19 +9,21 @@ import net.mamoe.mirai.utils.RemoteFile.Companion.uploadFile
 import java.io.File
 import kotlin.math.floor
 
+data class Trace(
+    var fileName: String? = null,
+    var tellerID: Long = 0L,
+    var tellerName: String? = null,
+    var recordString: String? = null)
+
 object Recorder {
-    private var fileName = ""
-    private var tellerID = 0L
-    private var tellerName = ""
-    private var recordingGroups : ArrayList<Long> = ArrayList()
-    private var recordString : HashMap<Long, String> = HashMap<Long, String> ()
+    private var record : HashMap<Long, Trace> = HashMap<Long, Trace> ()
     init {
         UniBot.qq.eventChannel.subscribeGroupMessages {
             always {
-                if (recordingGroups.contains(group.id) && tellerID == sender.id && tellerName == sender.nick) {
+                if (record.keys.contains(group.id) && record[group.id]?.tellerID == sender.id && record[group.id]?.tellerName == sender.nick) {
                     val msg = message.content
                     if (msg != "/end_record") {
-                        recordString.replace(group.id, recordString[group.id]+msg+"\n")
+                        record[group.id]?.recordString += msg + "\n"
                         subject.sendMessage("√")
                     }
                 }
@@ -29,28 +31,21 @@ object Recorder {
             }
             case("/start_record ") {
                 error {
-                    check(!recordingGroups.contains(group.id)) {"ERROR: another record is ongoing"}
-                    fileName = message.plainText().removePrefix("/start_record ")
-                    check(fileName.isNotEmpty()) { "ERROR: empty record name" }
-                    fileName += ".txt"
-                    tellerID = sender.id
-                    tellerName = sender.nick
-                    recordingGroups.add(group.id)
-                    recordString[group.id] = ""
-                    quoteReply("DOMO, ${tellerName}=san. Motor·Rainbow, recorder circuit on, 実際安い.")
+                    check(!record.keys.contains(group.id)) {"ERROR: another record is ongoing in this group"}
+                    var n = message.plainText().removePrefix("/start_record ")
+                    check(n.isNotEmpty()) { "ERROR: empty record name" }
+                    record[group.id] = Trace("$n.txt", sender.id, sender.nick, "")
+                    quoteReply("DOMO, ${record[group.id]?.tellerName}=san. Motor·Rainbow, recorder circuit on, 実際安い.")
                     }
                     return@case
                 }
             case("/end_record") {
-                if (sender.id == tellerID && sender.nick == tellerName) {
-                    var file = File.createTempFile(fileName, null)
-                    recordString[group.id]?.let { it1 -> file.writeText(it1) }
-                    group.filesRoot.resolve("/$fileName").uploadAndSend(file)
+                if (sender.id ==  record[group.id]?.tellerID && sender.nick ==  record[group.id]?.tellerName) {
+                    var file = File.createTempFile( record[group.id]?.fileName, null)
+                    record[group.id]?.recordString?.let { it1 -> file.writeText(it1) }
+                    group.filesRoot.resolve("/${record[group.id]?.fileName}").uploadAndSend(file)
                     var isDeleted = file.delete()
-                    tellerID = 0L
-                    tellerName = ""
-                    recordingGroups.remove(group.id)
-                    recordString.remove(group.id)
+                    record.remove(group.id)
                     subject.sendMessage("Recorder circuit off. Uploading...")
                 }
                 return@case
